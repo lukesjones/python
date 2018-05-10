abb5185eab5b29457ad1b549c1acfaed4b5215f6
lukesjones <lukesjones267@gmail.com>
2018-05-10 22:13:01 +0100

    lesson 1: string and int

---
+++ int.py
@@ -0,0 +1,11 @@
+i = 0
+input1 = input("what is the first number?")
+i = i + int(input1)
+input2 = input("what is the second number?")
+i = i - int(input2)
+input3 = input("what is the third number?")
+i = i * int(input3)
+input4 = input("what is the fourth number?")
+i = i / int(input4)
+print(int(i))
+
---
+++ string.py
@@ -0,0 +1,4 @@
+s="hello, "
+name = input("what is your name?")
+secondname = input("what is your second name?")
+print(s+name+" " +secondname)
