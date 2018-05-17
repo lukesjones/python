49f68748fe95f002b4b8c50e9d84d359adfe84cb
lukesjones <lukesjones267@gmail.com>
2018-05-17 21:18:07 +0100

    created add and minus functions

--- int.py
+++ int.py
@@ -1,11 +1,16 @@
 i = 0
-input1 = input("what is the first number?")
-i = i + int(input1)
-input2 = input("what is the second number?")
-i = i - int(input2)
-input3 = input("what is the third number?")
-i = i * int(input3)
-input4 = input("what is the fourth number?")
-i = i / int(input4)
-print(int(i))
 
+def add(quest):
+    global i
+    userInput = input(quest)
+    i = i + int(userInput)
+    print("the running total is", int(i))
+
+def minus(question):
+    global i
+    userInput = input(question)
+    i = i - int(userInput)
+
+add("what is the first number?")
+minus("what would you like to subtract?")
+print("the FINAL number is", int(i))
